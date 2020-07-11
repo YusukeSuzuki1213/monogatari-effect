8865469ac36da88a2626fae50573db93cca25af1
YusukeSuzuki1213 <cs16047@s.inf.shizuoka.ac.jp>
2020-07-11 12:24:16 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const MODE = "development";
+const enabledSourceMap = MODE === "development";
+
+module.exports = {
+  mode: MODE,
+  entry: `./src/index.ts`,
+
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'ts-loader',
+      },
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { 
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {              
+              sourceMap: enabledSourceMap
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  devServer: {
+    contentBase: "dist",
+    open: true
+  },
+
+  output: {
+    filename: "main.js"
+  },
+};
